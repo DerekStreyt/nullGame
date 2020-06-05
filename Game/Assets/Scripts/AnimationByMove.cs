a5d007e584937c9625fe0090a77bfbdb28315252
Derek <derek.streyt@gmail.com>
2020-06-05 13:11:01 +0300

    Add follow camera

---
+++ Game/Assets/Scripts/AnimationByMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class AnimationByMove : MonoBehaviour
+{
+    public Animator animator;
+    public float maxSpeed = 3;
+    public float lerpFactor = 4;
+    protected Vector3 lastPosition;
+    protected float moveValue01;
+    protected virtual void Awake()
+    {
+        animator = GetComponent<Animator>();
+        lastPosition = transform.position;
+        lastPosition.y = 0;
+    }
+
+    protected virtual void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        lastPosition.y = 0;
+        currentPosition.y = 0;
+
+        float v = Vector3.Distance(lastPosition, currentPosition);
+        float speed = v / Time.deltaTime;
+        float targetValue = Mathf.Abs(speed) < float.Epsilon ? 0 : speed / maxSpeed;
+        moveValue01 = Mathf.Lerp(moveValue01, targetValue, lerpFactor * Time.smoothDeltaTime);
+        animator.SetFloat("Speed", moveValue01);
+        lastPosition = currentPosition;
+    }
+}
---
+++ Game/Assets/Scripts/GameInput.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameInput : MonoBehaviour
+{
+    public enum EState
+    {
+        WASD,
+        Point
+    }
+
+    public Camera rayCamera;
+    public LayerMask mask;
+    public EState state;
+    public CharacterController characterController;
+    public NavMeshAgent agent;
+    public float speed = 3.5f;
+    public Vector3 moveDirection;
+
+    protected virtual void Start()
+    {
+        agent.speed = speed;
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button(state == EState.WASD ? "WASD" : "POINT"))
+        {
+            if (state == EState.Point)
+            {
+                state = EState.WASD;
+            }
+            else
+            {
+                state = EState.Point;
+            }
+        }
+    }
+
+    protected virtual void Update()
+    {
+     
+        switch (state)
+        {
+            case EState.WASD:
+                ApplyWASDMove();
+                break;
+            case EState.Point:
+                ApplyPointMove();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+    }
+
+    private void ApplyWASDMove()
+    {
+        agent.enabled = false;
+        characterController.enabled = true;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection.Normalize();
+        characterController.transform.LookAt(characterController.transform.position + moveDirection);
+        moveDirection *= speed;
+        moveDirection.y = -1;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void ApplyPointMove()
+    {
+        agent.enabled = true;
+        characterController.enabled = false;
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = rayCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, float.PositiveInfinity, mask))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (!agent.isStopped)
+        {
+            Vector3 direction = agent.velocity;
+            direction.Normalize();
+            direction.y = 0;
+            agent.transform.LookAt(agent.transform.position + direction);
+        }
+    }
+}
