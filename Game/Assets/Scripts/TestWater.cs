f2b11574a0459447092ec4907241c8c93f9d2d66
Derek <derek.streyt@gmail.com>
2020-06-05 15:18:20 +0300

    Add water

--- Game/Assets/Scripts/TestWater.cs
+++ Game/Assets/Scripts/TestWater.cs
@@ -4,11 +4,54 @@ using UnityEngine;
 
 public class TestWater : MonoBehaviour
 {
+    public int count = 10;
+    public float force = 10;
+    public float forceY = 3;
+    public Vector3 startVector = Vector3.up;
+    public float gravity = 0.98f;
     public Transform from;
     public Transform to;
     public LineRenderer lineRenderer;
+
     void Update()
     {
+        lineRenderer.positionCount = count;
 
+        Vector3 direction = (to.position - @from.position);
+        Debug.DrawRay(@from.position, direction * 10, Color.green);
+        lineRenderer.SetPosition(0, @from.position);
+        Vector3 prev = @from.position;
+        Vector3 gravityVector = startVector;
+
+        Vector3 forceVector = direction + Vector3.up * forceY * Time.fixedDeltaTime;
+        for (int i = 1; i < count; i++)
+        {
+
+            Vector3 next = prev + forceVector * (force * Time.fixedDeltaTime) + gravityVector;
+            gravityVector -= Vector3.up * gravity;
+            lineRenderer.SetPosition(i, next);
+            Debug.DrawLine(prev, next);
+            prev = next;
+        }
+        
+        /*
+        Vector3 a = Vector3.zero;
+        Vector3 v = Vector3.right;
+        Vector3 up = Vector3.up;
+        Vector3 g = up;
+        Vector3 prev1 = a;
+        for (int i = 1; i < count; i++)
+        {
+
+            Vector3 right = v + Vector3.up * forceY;
+
+            Vector3 next = prev1 + right * (force * Time.fixedDeltaTime) + g;
+
+            g -= Vector3.up;
+            
+            Debug.DrawLine(prev1, next);
+            Debug.DrawRay(prev1, Vector3.up, Color.cyan);
+            prev1 = next;
+        }*/
     }
 }
--- Game/Assets/Scripts/Unit.cs
+++ Game/Assets/Scripts/Unit.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    public int waterVertexCount = 50;
     public Vector3 Position => transform.position;
     public float rotateSpeed = 5;
     public Vector3 target;
+    public Vector3 startVector = Vector3.up;
+    public float force = 5;
+    public float forceY = 3;
+    public LineRenderer waterRenderer;
+    public Transform waterPivot;
+    public float gravity;
 
     protected virtual void Update()
     {
@@ -16,9 +23,35 @@ public class Unit : MonoBehaviour
             Quaternion lookRotation = Quaternion.LookRotation(lookAt);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed * Time.deltaTime);
         }
+
+        UseWater();
     }
+
     protected virtual Vector3 GetLookPoint()
     {
         return new Vector3(target.x, Position.y, target.z);
+    }
+
+    protected virtual void UseWater()
+    {
+        Vector3 prev = waterPivot.position;
+        waterRenderer.positionCount = waterVertexCount;
+        waterRenderer.SetPosition(0, prev);
+        Debug.DrawRay(prev, Vector3.up, Color.green);
+        Debug.DrawRay(target, Vector3.up, Color.red);
+        Vector3 direction = (target - prev);
+        direction.y = 0;
+        Vector3 forceVector = direction + Vector3.up * forceY * Time.fixedDeltaTime;
+        
+        float distance = Vector3.Distance(prev, target);
+        Vector3 gravityVector  = new Vector3(0, distance * 0.1f, 0); // todo
+        for (int i = 1; i < waterVertexCount; i++)
+        {
+            Vector3 next = prev + forceVector * (force * Time.fixedDeltaTime) + gravityVector;
+            gravityVector -= Vector3.up * gravity;
+            waterRenderer.SetPosition(i, next);
+            Debug.DrawLine(prev, next);
+            prev = next;
+        }
     }
 }
