dbbdee8c38feced415eca48d0d1d4ce5db896a7c
MrFlyingChip <kv_l@ukr.net>
2020-06-05 14:59:35 +0300

    Added GameManager and LevelController

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager
+{
+    public void StartGame()
+    {
+        
+    }
+    
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+
+            return _instance;
+        }
+    }
+}
