dd1bd4559e0e41972fceda581fdeb3a3a73f9549
Derek <derek.streyt@gmail.com>
2020-06-06 22:16:40 +0300

    Add civil logic

---
+++ Game/Assets/Scripts/Civil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Civil : Character
+{
+    public static int Count;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        Count++;
+    }
+
+    protected override void OnDie()
+    {
+        base.OnDie();
+        Count--;
+        Debug.Log("Civil was died left " + Count);
+        if (Count == 0)
+        {
+            GameManager.Instance.Lose();
+        }
+    }
+}
