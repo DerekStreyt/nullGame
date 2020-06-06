7dd23727441a9bbd24f6c294cc5088f6a9beff47
Derek <derek.streyt@gmail.com>
2020-06-06 16:50:54 +0300

    Add walls

---
+++ Game/Assets/Scripts/MessageUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageUI : MonoBehaviour
+{
+    protected Transform target;
+    public Vector3 offset;
+    protected RectTransform rectTransform;
+
+    protected virtual void Awake()
+    {
+        rectTransform = (RectTransform)transform;
+    }
+
+    protected virtual void Update()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(target.position+ offset);
+        rectTransform.position = pos;
+    }
+}
